eb41ae6510e314ffbc4030887cac5e35b310f67a
ribery <franck91@foxmail.com>
2018-02-17 22:08:21 +0900

    MainActivity get data from db

--- app/src/main/java/com/example/ribery/kakei/activity/MainActivity.java
+++ app/src/main/java/com/example/ribery/kakei/activity/MainActivity.java
@@ -2,16 +2,22 @@ package com.example.ribery.kakei.activity;
 
         import android.app.Activity;
         import android.content.Intent;
+        import android.database.Cursor;
+        import android.database.sqlite.SQLiteDatabase;
         import android.os.Bundle;
         import android.support.v7.widget.LinearLayoutManager;
         import android.support.v7.widget.RecyclerView;
+        import android.util.Log;
         import android.view.View;
+        import android.widget.ArrayAdapter;
         import android.widget.Button;
 
         import com.example.ribery.kakei.R;
         import com.example.ribery.kakei.activity.AddBillActivity;
         import com.example.ribery.kakei.adapter.Bill;
         import com.example.ribery.kakei.adapter.BillListAdapter;
+        import com.example.ribery.kakei.dbHelper.DBContract;
+        import com.example.ribery.kakei.dbHelper.DBHelper;
 
         import java.util.ArrayList;
 
@@ -20,11 +26,15 @@ public class MainActivity extends Activity {
     public RecyclerView rcv_bill_list;
     RecyclerView.LayoutManager layoutManager;
     BillListAdapter adapter;
+    DBHelper dbHelper;
+    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dbHelper = new DBHelper(this);
+        db = dbHelper.getReadableDatabase();
         initialViewItems();
     }
 
@@ -33,7 +43,7 @@ public class MainActivity extends Activity {
         rcv_bill_list = (RecyclerView) this.findViewById(R.id.rcv_bill_list);
         rcv_bill_list.setHasFixedSize(true);
         rcv_bill_list.setLayoutManager(layoutManager);
-        adapter = new BillListAdapter(getDummy());
+        adapter = new BillListAdapter(getDataFromDB());
         rcv_bill_list.setAdapter(adapter);
         btn_add_bill = (Button) this.findViewById(R.id.btn_main_add);
         btn_add_bill.setOnClickListener(new View.OnClickListener() {
@@ -55,4 +65,25 @@ public class MainActivity extends Activity {
         }
         return bills;
     }
+    private ArrayList<Bill> getDataFromDB () {
+        ArrayList<Bill> bills = new ArrayList<>();
+        Cursor cursor = db.query(DBContract.KakeiEntry.TABLE_NAME, null, null, null, null, null, null);
+        Bill bill;
+        while (cursor.moveToNext()){
+            String date = cursor.getString(cursor.getColumnIndex(DBContract.KakeiEntry.COLUMN_NAME_DATE));
+            String genre = cursor.getString(cursor.getColumnIndex(DBContract.KakeiEntry.COLUMN_NAME_GENRE));
+            int money = cursor.getInt(cursor.getColumnIndex(DBContract.KakeiEntry.COLUMN_NAME_MONEY));
+            String about = cursor.getString(cursor.getColumnIndex(DBContract.KakeiEntry.COLUMN_NAME_ABOUT));
+
+            Log.d("Query date ", date);
+            Log.d("Query genre ", genre);
+            Log.d("Query money ", money + "");
+            Log.d("Query about", about);
+            bill = new Bill(date, genre, about, money + "");
+            bills.add(bill);
+        }
+        cursor.close();
+        return bills;
+    }
+
 }
